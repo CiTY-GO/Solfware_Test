9973da141d6a9c0b76a7817d23f129af495d68d4
qcloud <ubuntu@localhost.localdomain>
2023-03-26 15:16:55 +0800

    Modify the autotest.py

--- scripts/autotest.py
+++ scripts/autotest.py
@@ -10,13 +10,14 @@ def run_by_version():
         BASE_PATH = '/home/ubuntu/RGtest/grep'
         # 源代码目录路径
         SRC_PATH = os.path.join(BASE_PATH, 'versions.alt/versions.seeded/v1/')
+        ORG_PATH = os.path.join(BASE_PATH, 'versions.alt/versions.orig/v1/')
         # 植入宏（命令行参数）文件路径
         FAULT_PATH = os.path.join(BASE_PATH, 'versions.alt/versions.seeded/v1/FaultSeeds.h')
         # 输入数据（命令行参数）文件路径
         INPUT_FILE_PATH = os.path.join(BASE_PATH, 'testplans.alt/v1/v0.cov.universe')
         
 
-        for code_version in range(20):
+        for code_version in range(19):
             #version19 为 未植入错误宏的版本
             if code_version != 0:
                 continue
@@ -25,7 +26,10 @@ def run_by_version():
             # 日志文件路径
             LOG_FILE_PATH = os.path.join(OUTPUT_PATH, f'log{code_version}.txt')
             
-            
+            #diff 文件路径
+            Diff_FILE_PATH = os.path.join(OUTPUT_PATH, f'diff.txt')
+            Diff_1_FILE_PATH = os.path.join(OUTPUT_PATH, f'diff1.txt')
+
             params = parse_params(INPUT_FILE_PATH)
             
             # 1. 植入错误宏 
@@ -38,22 +42,32 @@ def run_by_version():
                 rm -f log{code_version}.txt;
                 mkdir version{code_version};
                 cd ./version{code_version};
-                mkdir out;
                 {echo_green(f'START code version {code_version}')}
             ''')
-            os.system(f'''
+            
+            OUTPUT_PATH = os.path.join(BASE_PATH, f'outputs.alt/v1/version{code_version}')
+            
+            for i in range(len(params)):
+                if i > 50:
+                    continue
+                os.system(f'''
+                cd {OUTPUT_PATH};
+                mkdir {i}
+                ''')
+                Right_cmd_PATH = os.path.join(OUTPUT_PATH, f'{i}/test0.txt')
+                Error_cmd_PATH = os.path.join(OUTPUT_PATH, f'{i}/test.txt')
+                Static_info_PATH = os.path.join(OUTPUT_PATH, f'{i}/Static_info.txt')
+                Comp_info_PATH = os.path.join(OUTPUT_PATH, f'{i}/comp_info.txt')
+                
+                # print(params[i])
+                # os.system(f'echo {params[i]}')
+
+                os.system(f'''
                 cd {SRC_PATH};          
                 make build;
                 {echo_green('COMPILE finished. generated [grep.exe] [grep.gcno]')}
-            ''')
-            OUTPUT_PATH = os.path.join(BASE_PATH, f'outputs.alt/v1/version{code_version}')
-            LOG_FILE_PATH2 = os.path.join(OUTPUT_PATH, f'out/temp2.txt')
-            LOG_FILE_PATH3 = os.path.join(OUTPUT_PATH, f'out/temp3.txt')
-            for i in range(len(params)):
-                if i > 3:
-                    continue
-                print(params[i])
-                # os.system(f'echo {params[i]}')
+                ''')
+                
                 os.system(f'''
                     echo "BEGIN_INPUT" >> {LOG_FILE_PATH};
                     echo {params[i]} >> {LOG_FILE_PATH};
@@ -61,49 +75,37 @@ def run_by_version():
 
                     cd {os.path.join(BASE_PATH, 'scripts')};
                     echo "RUN {i}" >> {LOG_FILE_PATH};
-                    {os.path.join(SRC_PATH, 'grep.exe')} {params[i]} < ./stdin.txt >> {LOG_FILE_PATH};
-                    {echo_green('RUN finished. generated [grep.gcda]')}
+                    {os.path.join(SRC_PATH, 'grep.exe')} {params[i]} < ./stdin.txt >> {Error_cmd_PATH};
+                    {os.path.join(ORG_PATH, 'grep.exe')} {params[i]} < ./stdin.txt >> {Right_cmd_PATH};
+                    {echo_green('RUN finished. generated [grep.gcda]')}>>{LOG_FILE_PATH};
                     
                     
                     cd {SRC_PATH};
                     echo "TEST {i}" >> {LOG_FILE_PATH};
-                    gcov grep.c >> {LOG_FILE_PATH};
+                    gcov -bfr grep.c >> {Static_info_PATH};
 
-                
-                    lcov -d . -c -o {i}.info --rc lcov_branch_coverage=1 >>{LOG_FILE_PATH2};
-                    mv {i}.info {os.path.join(OUTPUT_PATH,f'out/')};
-
+                    echo "" >> {Diff_FILE_PATH}
+                    
+                    echo "{code_version} -th fault and the {i} case " >> {Diff_FILE_PATH}
+                    diff {Right_cmd_PATH} {Error_cmd_PATH} 
+                    diff {Right_cmd_PATH} {Error_cmd_PATH} | tee -a {Comp_info_PATH} {Diff_FILE_PATH}
+                    cmp --silent {Right_cmd_PATH} {Error_cmd_PATH} || echo "{code_version} -th fault and the {i} case failed " >> {Diff_1_FILE_PATH}
 
                     {echo_green('TEST finished.(with branch coverage) generated [grep.c.gcov] [wchar.h.gcov]')}
                     
                     echo >> {LOG_FILE_PATH};
                 ''')
-                if i == 0:
-                    os.system(f'''
-                    mk {os.path.join(OUTPUT_PATH,f'out/')}out.info
-                    ''')
-                elif i == 1:
-                     os.system(f'''
-                    lcov -a  {os.path.join(OUTPUT_PATH,f'out/0.info')} -a {os.path.join(OUTPUT_PATH,f'out/0.info')} -o {os.path.join(OUTPUT_PATH,f'out/')}out.info --rc lcov_branch_coverage=1 >>{LOG_FILE_PATH3};
-                     ''')
-                else:
-                     os.system(f'''
-                    lcov -a  {os.path.join(OUTPUT_PATH,f'out/out.info')} -a {os.path.join(OUTPUT_PATH,f'out/')}{i}.info -o {os.path.join(OUTPUT_PATH,f'out/')}out.info --rc lcov_branch_coverage=1 >>{LOG_FILE_PATH3};
-                     ''')
+                
 
                 os.system(f'''
                     cd {SRC_PATH};
                     mv ./grep.c.gcov {os.path.join(OUTPUT_PATH, f'grep.c.{code_version}_{i}.gcov')};
                     {echo_green('MOVE [grep.c.gcov] to output directory')}
-                    rm ./grep.gcda ./wchar.h.gcov;
+                    rm ./grep.gcda ;
                     {echo_green('CLEAR finished')}
                     {echo_green(f'[CASE {i} DONE]')}
                 ''')
 
-                if i ==198:
-                    os.system(f'''
-                        genhtml --branch-coverage {os.path.join(OUTPUT_PATH,f'out/')}out.info --output-directory {os.path.join(OUTPUT_PATH,f'out/out')}{code_version};
-                    ''')
             os.system(f'''
                     cd {SRC_PATH};
                     rm ./grep.exe ./grep.gcno
