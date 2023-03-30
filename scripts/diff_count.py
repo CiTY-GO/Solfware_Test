8d262f5d2c73c7d4d7e963797d0c30d3710344b2
qcloud <ubuntu@localhost.localdomain>
2023-03-30 20:49:29 +0800

    Diff done , Visualization done , all complete

--- scripts/diff_count.py
+++ scripts/diff_count.py
@@ -3,12 +3,7 @@ import os
 
 
 def exist(file_path):
-    if os.path.exists(file_path):
-        return True
-    else:
-        return False
-
-
+    return os.path.exists(file_path)
 
 def diff_count(filename1: str, filename2: str) -> int:
     with open(filename1, 'r') as file1, open(filename2, 'r') as file2:
@@ -22,18 +17,24 @@ def diff_count(filename1: str, filename2: str) -> int:
 def diff_version(v1, v2):
     BASE_PATH = '/home/ubuntu/RGtest/grep/outputs.alt/v1/'
     num_of_testcase = 199
-
+    not_exist_indices = []
+    right_cnt = 0
     for i in range(num_of_testcase):
         
         v1_path = os.path.join(BASE_PATH, f'version_{v1}/exec/version_{v1}_{i}.txt')
         v2_path = os.path.join(BASE_PATH, f'version_{v2}/exec/version_{v2}_{i}.txt')
-        if exist(v1_path) == False or exist(v2_path) == False:
+        if not exist(v1_path) or not exist(v2_path):
+            not_exist_indices.append(i)
             continue
         cnt = diff_count(v1_path, v2_path)
-        if cnt != 0:
-            print(f'diff count case{i}: V{v1} vs V{v2} => {cnt}')
+        if cnt == 0:
+            right_cnt += 1
 
+    return [right_cnt, num_of_testcase - len(not_exist_indices)]
 
 if __name__ == '__main__':
-    diff_version(0, 10)
-            
+    right_version = 18
+    diff_data = []
+    for i in range(18):
+        diff_data.append(diff_version(i, right_version))
+    print(diff_data)
